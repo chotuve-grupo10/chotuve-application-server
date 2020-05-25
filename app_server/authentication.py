3f986e406ebe098581667ca2062a67435f4f24ee
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-05-25 12:15:30 -0300

    Obviamente pytlint tenia que romper las bolas

--- app_server/authentication.py
+++ app_server/authentication.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Blueprint, current_app, request
+from flask import Blueprint
 from flasgger import swag_from
 
 authentication_bp = Blueprint('authentication', __name__)
@@ -8,31 +8,29 @@ logger = logging.getLogger('gunicorn.error')
 @authentication_bp.route('/api/register/', methods=['POST'])
 @swag_from('docs/register.yml')
 def _register_user():
-    #user_request = request.headers['AuthenticationHeader']
-    #response_auth_server = post_auth_server_register(os.environ.get('AUTH_SERVER_URL'), user_request)
-    return {}
+	return {}
 
 @authentication_bp.route('/api/register_with_facebook/', methods=['POST'])
 @swag_from('docs/register_with_facebook.yml')
 def _register_user_using_facebook():
-    return {}
+	return {}
 
 @authentication_bp.route('/api/register_with_google/', methods=['POST'])
 @swag_from('docs/register_with_google.yml')
 def _register_user_using_google():
-    return {}
+	return {}
 
 @authentication_bp.route('/api/login/', methods=['POST'])
 @swag_from('docs/login.yml')
 def _login_user():
-    return {}
+	return {}
 
 @authentication_bp.route('/api/login_with_facebook/', methods=['POST'])
 @swag_from('docs/login_with_facebook.yml')
 def _login_user_using_facebook():
-    return {}
+	return {}
 
 @authentication_bp.route('/api/login_with_google/', methods=['POST'])
 @swag_from('docs/login_with_google.yml')
 def _login_user_using_google():
-    return {}
+	return {}
