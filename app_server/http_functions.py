62989c5605fdff6cf5d4e485f2c1ea36c3eaa838
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-05-27 20:46:41 -0300

    Mas tests wachin

--- app_server/http_functions.py
+++ app_server/http_functions.py
@@ -6,9 +6,9 @@ logger = logging.getLogger('gunicorn.error')
 
 def get_auth_server_ping(url_received):
 	logger.debug('Auth server ping requested')
-	#TODO: manejar error si url viene vacia
-	if url_received is None:
+	if not url_received:
 		logger.critical("URL received is empty")
+		raise ValueError('URL received is empty')
 	else:
 		logger.debug('URL: ' + url_received)
 	response = requests.get(url=url_received)
@@ -16,9 +16,9 @@ def get_auth_server_ping(url_received):
 
 def get_media_server_ping(url_received):
 	logger.debug('Media server ping requested')
-	#TODO: manejar error si url viene vacia
-	if url_received is None:
+	if not url_received:
 		logger.critical("URL received is empty")
+		raise ValueError('URL received is empty')
 	else:
 		logger.debug('URL: ' + url_received)
 	response = requests.get(url=url_received)
@@ -26,12 +26,14 @@ def get_media_server_ping(url_received):
 
 def post_auth_server(url, user_data):
 	logger.debug('Auth server register requested')
-	#TODO: manejar error si url viene vacia
-	if url is None:
+	if not url:
 		logger.critical("URL received is empty")
-	else:
-		logger.debug('URL: ' + url)
-	# la espera de esta respuesta es bloqueante?
+		raise ValueError('URL received is empty')
+	if user_data is None:
+		logger.critical("User data is None")
+		raise ValueError("User data is None")
+	
+	logger.debug('URL: ' + url)
 	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 	response = requests.post(url=url, data=json.dumps(user_data), headers=headers)
 	return response
--- tests/test_http_functions.py
+++ tests/test_http_functions.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 import simplejson as json
+import pytest
 from app_server import http_functions
 
 def test_auth_server_ping_successfully(client):
@@ -8,3 +9,28 @@ def test_auth_server_ping_successfully(client):
 		response = http_functions.get_auth_server_ping('test')
 
 		assert response.status_code == 200
+
+def test_auth_server_ping_error_url_empty(client):
+	with pytest.raises(ValueError) as e:
+		 http_functions.get_auth_server_ping('')
+	assert str(e.value) == 'URL received is empty'
+
+def test_auth_media_ping_error_url_empty(client):
+	with pytest.raises(ValueError) as e:
+		http_functions.get_media_server_ping('')
+	assert str(e.value) == 'URL received is empty'
+
+def test_auth_server_ping_error_url_empty(client):
+	with pytest.raises(ValueError) as e:
+		 http_functions.get_auth_server_ping('')
+	assert str(e.value) == 'URL received is empty'
+
+def test_post_auth_server_error_url_empty(client):
+	with pytest.raises(ValueError) as e:
+		http_functions.post_auth_server('', None)
+	assert str(e.value) == 'URL received is empty'
+
+def test_post_auth_server_error_data_empty(client):
+	with pytest.raises(ValueError) as e:
+		http_functions.post_auth_server('test', None)
+	assert str(e.value) == 'User data is None'
