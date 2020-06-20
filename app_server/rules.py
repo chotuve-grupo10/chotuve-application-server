c24bd362a87ea2df85a9a1c5e6cd409d04ffd0df
nicolaslongo <nicolaslongo@github.com>
2020-06-19 23:21:57 -0300

    Agrego funciones juguete y tests. No se entiende una garompa, hay que decirlo

---
+++ app_server/rules.py
@@ -0,0 +1,38 @@
+from durable.lang import *
+
+with ruleset('comments_test'):
+	@when_all(m.comments == '2')
+	def comment_is_two(c):
+		logger.debug('All comments are 2s')
+
+
+with ruleset('choice_of_sequences'):
+	@when_any(all(c.first << m.comments == '2',
+				  c.second << m.likes == '5'),
+			  all(c.first << m.comments == '5',
+				  c.second << m.likes == 2))
+
+	def action(c):
+		msg = 'Comentarios es 2 y likes es 5'
+		logger.debug(msg)
+		return msg
+
+with ruleset('one_nesting'):
+	@when_all(c.user << (m.user == 'diegote') & (m.videos_stats.quantity > 10))
+	def action(c):
+		msg = 'Use un nivel de nesting'
+		logger.debug(msg)
+		return msg
+
+with ruleset('array_matching'):
+	@when_all(m.videos.allItems(item.comments < 5))
+	def rule_1(c):
+		return '{0} matches rule 1'
+
+	@when_all(m.videos.allItems((item.comments >= 5) & (item.comments < 10)))
+	def rule_2(c):
+		return '{0} matches rule 2'
+
+	@when_all(m.videos.allItems((item.comments >= 10) & (item.comments < 25)))
+	def rule_2(c):
+		return '{0} matches rule 2'
