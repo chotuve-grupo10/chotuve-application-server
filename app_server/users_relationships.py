65ff51b8db004dc6d5662863a027e71d390d0745
nicolaslongo <nicolaslongo@github.com>
2020-06-20 17:55:10 -0300

    pylint again

--- app_server/users_relationships.py
+++ app_server/users_relationships.py
@@ -1,10 +1,11 @@
+# pylint: disable=C0103
 import os
 from pymongo import MongoClient
 
 client = MongoClient(os.environ.get('DATABASE_URL'))
 
 def test_database():
-	db = 'appServer'
+	db = 'app_server'
 	coll = 'amistades'
 	amistades = client[db][coll]
-	amistades.insert_one({'this_is': 'tercera_pruebita'})
+	amistades.insert_one({'this_is': 'primera_prueba_nueva_db'})
