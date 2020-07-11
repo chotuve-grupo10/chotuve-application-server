3ab42868f928df10ae4d91ee5060a3445fa2eb42
Diego Alfonso <diego.alfonso2@gmail.com>
2020-07-11 05:14:35 -0300

    Agrego exposicion de metricas de gunicorn a DataDog

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 datadog-agent run &
 /opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml &
 /opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml &
-gunicorn --log-level=debug 'app_server:create_app()'
+gunicorn --log-level=debug --statsd-host=localhost:8125 --name=chotuve-app-server 'app_server:create_app()'
