ea96f0586500826b4fd5e2743262d4e1a4cf5cb2
Diego Alfonso <diego.alfonso2@gmail.com>
2020-07-11 04:40:05 -0300

    Agrego exposicion de metricas de Docker hacia DataDog

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+datadog-agent run &
+/opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml &
+/opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml &
+gunicorn --log-level=debug 'app_server:create_app()'
