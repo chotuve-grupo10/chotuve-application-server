b6a5c8da2814b0307f9f00ff63086baeae160912
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-07-18 16:11:55 -0300

    Arreglo las quejas del gede de pylint

--- tests/test_token_functions.py
+++ tests/test_token_functions.py
@@ -1,14 +1,14 @@
 from app_server import token_functions
 
 def test_decode_token_successfully():
-    user_data = {'email' : 'test@test.com'}
-    token = token_functions.generate_app_token(user_data)
+	user_data = {'email' : 'test@test.com'}
+	token = token_functions.generate_app_token(user_data)
 
-    result, status_code = token_functions.validate_token(token)
+	_result, status_code = token_functions.validate_token(token)
 
-    assert status_code == 200
+	assert status_code == 200
 
 def test_is_not_valid_token():
-    result, status_code = token_functions.validate_token('testststs')
+	_result, status_code = token_functions.validate_token('testststs')
 
-    assert status_code == 401
+	assert status_code == 401
