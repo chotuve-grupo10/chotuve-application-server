809ded61d227b1d1f5ed7a4ec10750415d886d36
nicolaslongo <nicolaslongo@github.com>
2020-07-18 18:28:02 -0300

    Cambios minimos

--- app_server/videos_reactions.py
+++ app_server/videos_reactions.py
@@ -13,11 +13,9 @@ videos_reactions_bp = Blueprint('videos_reactions', __name__)
 logger = logging.getLogger('gunicorn.error')
 
 client = MongoClient(os.environ.get('DATABASE_URL'))
-DB = 'app_server'
-
 @videos_reactions_bp.route('/api/videos/<video_id>/likes', methods=['POST'])
-@swag_from('docs/like_video.yml')
 @auth_required
+@swag_from('docs/like_video.yml')
 def _like_video(video_id):
 	user_email = g.data['user_id']
 
@@ -42,38 +40,12 @@ def _like_video(video_id):
 
 	return result, status_code
 
-
-@videos_reactions_bp.route('/api/videos/<video_id>/likes', methods=['DELETE'])
-@swag_from('docs/delete_like_to_video.yml')
-@auth_required
-def _delete_like_video(video_id):
-	user_email = g.data['user_id']
-
-	coll_users = 'users'
-	user = get_user_by_email(user_email, client[DB][coll_users])
-	if user is None:
-		logger.error('El usuario %s que quiso likear un video no existe', user_email)
-		return {'Delete_like_video': 'User not found'}, HTTP_NOT_FOUND
-
-	coll_videos = 'videos'
-	status_code = delete_like_video(video_id, user, client[DB][coll_videos])
-
-	if status_code == HTTP_OK:
-		video = get_video_for_response(video_id, client[DB][coll_videos])
-		result = json.dumps(video, default=json_util.default)
-	elif status_code == HTTP_CONFLICT:
-		result = {'Delete_like_video': 'Este video no tenia tu like'}
-	elif status_code == HTTP_NOT_FOUND:
-		result = {'Delete_like_video': 'No se encontró el video'}
-	else:  # HTTP_INTERNAL_SERVER_ERROR
-		result = {'Delete_like_video': 'Sucedió un error'}
-
-	return result, status_code
+DB = 'app_server'
 
 
 @videos_reactions_bp.route('/api/videos/<video_id>/dislikes', methods=['POST'])
-@swag_from('docs/dislike_video.yml')
 @auth_required
+@swag_from('docs/dislike_video.yml')
 def _dislike_video(video_id):
 	user_email = g.data['user_id']
 
@@ -99,9 +71,37 @@ def _dislike_video(video_id):
 	return result, status_code
 
 
-@videos_reactions_bp.route('/api/videos/<video_id>/dislikes', methods=['DELETE'])
-@swag_from('docs/delete_dislike_to_video.yml')
+@videos_reactions_bp.route('/api/videos/<video_id>/likes', methods=['DELETE'])
 @auth_required
+@swag_from('docs/delete_like_to_video.yml')
+def _delete_like_video(video_id):
+	user_email = g.data['user_id']
+
+	coll_users = 'users'
+	user = get_user_by_email(user_email, client[DB][coll_users])
+	if user is None:
+		logger.error('El usuario %s que quiso likear un video no existe', user_email)
+		return {'Delete_like_video': 'User not found'}, HTTP_NOT_FOUND
+
+	coll_videos = 'videos'
+	status_code = delete_like_video(video_id, user, client[DB][coll_videos])
+
+	if status_code == HTTP_OK:
+		video = get_video_for_response(video_id, client[DB][coll_videos])
+		result = json.dumps(video, default=json_util.default)
+	elif status_code == HTTP_CONFLICT:
+		result = {'Delete_like_video': 'Este video no tenia tu like'}
+	elif status_code == HTTP_NOT_FOUND:
+		result = {'Delete_like_video': 'No se encontró el video'}
+	else:  # HTTP_INTERNAL_SERVER_ERROR
+		result = {'Delete_like_video': 'Sucedió un error'}
+
+	return result, status_code
+
+
+@videos_reactions_bp.route('/api/videos/<video_id>/dislikes', methods=['DELETE'])
+@auth_required
+@swag_from('docs/delete_dislike_to_video.yml')
 def _delete_dislike_video(video_id):
 	user_email = g.data['user_id']
 
