eb2eeae10b82f94ab048d20c9b16cd044a8af574
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-07-27 12:05:06 -0300

    Un test en reactions

--- tests/test_videos_reactions.py
+++ tests/test_videos_reactions.py
@@ -1,6 +1,8 @@
 # pylint: disable=C0411
+import simplejson as json
 from app_server.videos_reactions_db_functions import *
 from app_server.videos_db_functions import insert_video_into_db
+from app_server.token_functions import generate_app_token
 from pymongo_inmemory import MongoClient
 
 DB = 'test_app_server'
@@ -17,6 +19,26 @@ empty_video_01 = {
 }
 
 #### Like tests ####
+
+def test_like_fails_user_not_found(client):
+	client_db = MongoClient()
+	collection = client_db[DB]['videos']
+
+	status_code = insert_video_into_db('5df1679ee0acd518e5cfd002', empty_video_01, collection)
+	assert status_code == HTTP_CREATED
+
+	result = list(collection.find({}))
+	assert len(result) == 1
+
+	fake_user = {'email': 'fake_email@gmail.com'}
+	token = generate_app_token(fake_user)
+	response = client.post('/api/videos/22222225ef1679ee0acd518e/likes',
+								headers={'Authorization': token},
+								follow_redirects=False)
+
+	assert response.status_code == HTTP_NOT_FOUND
+	assert json.loads(response.data) == {'Like_video': 'User not found'}
+	client_db.close()
 
 def test_video_not_found_fails():
 	client = MongoClient()
