a8ff98125054f2dfeaea7d23a98ebf4a219ba322
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-07-29 20:12:56 -0300

    Arreglo los tests

--- tests/test_videos.py
+++ tests/test_videos.py
@@ -118,7 +118,8 @@ def test_get_videos_from_specific_user_fails_invalid_token(client):
 		mock.return_value = {'Message': 'invalid token'}, 401
 
 		user = 'test@test.com'
-		response = client.get('/api/users/' + user + '/videos/',
+
+		response = client.get('/api/users/videos/', query_string={'user_id':user},
 							   headers={'Authorization': 'FAKETOKEN'},
 							   follow_redirects=False)
 		assert mock.called
@@ -145,7 +146,7 @@ def test_get_videos_from_specific_user_with_users_being_friends(client):
 					user_to_get_videos = 'test2@test.com'
 
 					token = generate_app_token({'email': user_requesting})
-					response = client.get('/api/users/' + user_to_get_videos + '/videos/',
+					response = client.get('/api/users/videos/', query_string={'user_id':user_to_get_videos},
 										headers={'Authorization': token},
 										follow_redirects=False)
 
@@ -178,7 +179,7 @@ def test_get_videos_from_specific_user_with_users_not_being_friends(client):
 					user_to_get_videos = 'test2@test.com'
 
 					token = generate_app_token({'email': user_requesting})
-					response = client.get('/api/users/' + user_to_get_videos + '/videos/',
+					response = client.get('/api/users/videos/', query_string={'user_id':user_to_get_videos},
 										headers={'Authorization': token},
 										follow_redirects=False)
 
@@ -206,7 +207,7 @@ def test_get_own_videos_successfully(client):
 				mock_append_data.return_value = [video_info, video_info2]
 
 				token = generate_app_token({'email': user_requesting})
-				response = client.get('/api/users/' + user_requesting + '/videos/',
+				response = client.get('/api/users/videos/', query_string={'user_id':user_requesting},
 									headers={'Authorization': token},
 									follow_redirects=False)
 
